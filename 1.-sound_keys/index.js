7ef5b9a9d4eecda405300237e3df3cf450a3330a
Amderson Hernandez <ahernandezdep@gmail.com>
2023-12-20 22:36:19 -0500

    "si"

--- 1.-sound_keys/index.js
+++ 1.-sound_keys/index.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //stop de ejecution
@@ -6,10 +6,13 @@ window.addEventListener("keydown", function (e) {
   audio.play();
 
   key.classList.add("playing");
-});
+}
 function removeTransition(e) {
-  console.log(e);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);

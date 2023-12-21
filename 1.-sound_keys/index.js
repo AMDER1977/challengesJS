9ca67af3a51c81b47c931191537eeaeeb1f34ed8
Amderson Hernandez <ahernandezdep@gmail.com>
2023-12-20 22:02:17 -0500

    "estilos simples"

--- 1.-sound_keys/index.js
+++ 1.-sound_keys/index.js
@@ -1,3 +1,8 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stop de ejecution
+  audio.currentTime = 0; //rewind to start
+  audio.play();
+  console.log(key);
 });
